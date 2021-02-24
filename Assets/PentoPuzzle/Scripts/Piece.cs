3ff4e03bdcd5ddb0992c1ebc35cc51e88ecec444
Cooper Saye <coopersaye@gmail.com>
2021-02-24 16:28:58 -0700

    Fix drag position offset.

--- Assets/PentoPuzzle/Scripts/Piece.cs
+++ Assets/PentoPuzzle/Scripts/Piece.cs
@@ -6,6 +6,9 @@ namespace PentoPuzzle
     {
         private Camera mainCamera;
 
+        private bool offsetSet = false;
+        private Vector2 mouseOffset;
+
         private void Start()
         {
             // Cache main camera reference
@@ -16,11 +19,23 @@ namespace PentoPuzzle
         private void OnMouseDrag()
         {
             Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            // Set mouse offset if not set
+            if (!offsetSet)
+            {
+                offsetSet = true;
+                mouseOffset = (Vector2)transform.position - mousePosition;
+            }
             // Round mouse position to snap to grid
-            float x = Mathf.Round(mousePosition.x);
-            float y = Mathf.Round(mousePosition.y);
+            float x = Mathf.Round(mousePosition.x + mouseOffset.x);
+            float y = Mathf.Round(mousePosition.y + mouseOffset.y);
             // Move to rounded mouse position
             transform.position = new Vector2(x, y);
+        }
+
+        private void OnMouseUp()
+        {
+            // Reset mouse offset on mouse up
+            offsetSet = false;
         }
     }
 }
