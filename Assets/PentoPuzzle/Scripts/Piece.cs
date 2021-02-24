a42612185ddcd6ca7f1409d2857a93ff5f6da248
Cooper Saye <coopersaye@gmail.com>
2021-02-23 20:38:40 -0700

    Implement piece dragging.

---
+++ Assets/PentoPuzzle/Scripts/Piece.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace PentoPuzzle
+{
+    public class Piece : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Camera mainCamera = null;
+
+        // Move to mouse position on drag
+        public void OnMouseDrag()
+        {
+            Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            // Round mouse position to snap to grid
+            float x = Mathf.Round(mousePosition.x);
+            float y = Mathf.Round(mousePosition.y);
+            // Move to rounded mouse position
+            transform.position = new Vector2(x, y);
+        }
+    }
+}
