d2397405bb59f676b60d5ebfb8d201e1fa9e01fb
Cooper Saye <coopersaye@gmail.com>
2021-02-26 19:46:18 -0700

    Create reset button.

---
+++ Assets/PentoPuzzle/Scripts/ResetButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PentoPuzzle
+{
+    public class ResetButton : MonoBehaviour
+    {
+        // Reloads current scene
+        public void ReloadScene()
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentSceneIndex);
+        }
+    }
+}
